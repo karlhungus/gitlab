eb4d711850d62703d868254f0106efeec09934d8
Izaak Alpert <ihalpert@rim.com>
2013-03-13 17:23:23 -0400

    Had a bug with the post protocol
    erroniously called group.id group_id in the body of the message when it is supposed to be id
    
    GIT-1972

--- lib/gitlab/client/groups.rb
+++ lib/gitlab/client/groups.rb
@@ -23,7 +23,7 @@ class Gitlab::Client
     # @param  [Integer] group_id
     # @param  [Integer] project_id
     def transfer_project_to_group(group_id, project_id)
-      body = {:group_id => group_id, :project_id => project_id}
+      body = {:id => group_id, :project_id => project_id}
       post("/groups/#{group_id}/projects/#{project_id}", :body => body)
     end
 
